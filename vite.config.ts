e20a0c2206e2556d51321329435ba8481c86226b
Hafis Mohammad <hafismhdthaleekara764@gmail.com>
2024-12-18 21:31:44 +0530

    Bug fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc'; // This plugin allows fast SWC-based React development with Vite
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-});
+})
