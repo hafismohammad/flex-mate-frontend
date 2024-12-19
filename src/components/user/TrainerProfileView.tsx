99feb9e196a1f6cbdc5c9c5e6a410cfcfbd3e23c
Hafis Mohammad <hafismhdthaleekara764@gmail.com>
2024-12-19 18:55:44 +0530

    baseurl changes

--- src/components/user/TrainerProfileView.tsx
+++ src/components/user/TrainerProfileView.tsx
@@ -44,7 +44,7 @@ function TrainerProfileView() {
     const fetchTrainer = async () => {
       try {
         const response = await axios.get(
-          `https://www.flexmate.space/api/user/trainers/${trainerId}`
+          `https://api.flexmate.space/api/user/trainers/${trainerId}`
         );
         setTrainer(response.data[0]);
       } catch (error) {
