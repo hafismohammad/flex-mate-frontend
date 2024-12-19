eedc966f4d06be4f40766c0403c44d757c28e6ea
Hafis Mohammad <hafismhdthaleekara764@gmail.com>
2024-12-19 19:07:49 +0530

    baseurl correction

--- src/components/user/TrainerProfileView.tsx
+++ src/components/user/TrainerProfileView.tsx
@@ -44,7 +44,7 @@ function TrainerProfileView() {
     const fetchTrainer = async () => {
       try {
         const response = await axios.get(
-          `${import.meta.env.VITE_BASE_URL}/user/trainers/${trainerId}`
+          `${import.meta.env.VITE_BASE_URL}/api/users/trainers/${trainerId}`
         );
         setTrainer(response.data[0]);
       } catch (error) {
